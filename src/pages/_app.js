4c423281643ec22ce9a9de71e187bdaabf8f47d6
MINTRA SRIVICHAI <64050620@kmitl.ac.th>
2025-04-16 20:29:44 +0700

    Fix: Add missing semicolons in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,6 @@ import "@/styles/globals.css";
 import './Droneconfig.css';
 import './re.css';
 import './temperature.css';
-git init
- 
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
