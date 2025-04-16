d6a9daf1f67840c9e3f6a92c81753904cfa0d939
MINTRA SRIVICHAI <64050620@kmitl.ac.th>
2025-04-16 21:27:57 +0700

    fix: removed global CSS imports from pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
-import './Droneconfig.css';
-import './re.css';
-import './temperature.css';
+import "./Droneconfig.css";
+import "./re.css";
+import "./temperature.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
